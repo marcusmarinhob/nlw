1c282b0916084fd6ed0cdd31e16e9dd63529985c
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2020-06-04 12:56:44 -0300

    Introdução ao Javascript

---
+++ scripts/intro.js
@@ -0,0 +1,71 @@
+// document.write("Hello")
+
+// variaveis, tipos de dados
+// var myvar = "He"
+// let myvar2 = "He"
+// const myconst = "He"
+
+// document.write(myvar)
+// document.write(myconst)
+// document.write(myconst + myvar)
+
+// document.write("Isso é uma string")
+// document.write('Isso é uma string')
+// document.write(`Isso é uma string`)
+
+// const n1 = 1
+// const n2 = 2
+// document.write(n1 + n2)
+
+// const bTrue = true
+// const bFalse = false
+
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+  
+//   correr(){
+//     return "Corre, bebê"
+//   }
+// }
+
+// document.write(pessoa.correr())
+
+// const colecaoDeBolinhas = ["blue", "green", 1, {name: "My name"}]
+                           
+// document.write(colecaoDeBolinhas[3].name)                          
+
+
+// Funções - Funcionalidades - Atalhos - Reuso de código
+// function sayMyName(name){
+//   document.write(name)
+// }
+
+// sayMyName("Marcus")
+// sayMyName("Marcus")
+// sayMyName("Marcus")
+// sayMyName("Marcus")
+// sayMyName("Marcus")
+// sayMyName("Marcus")
+// sayMyName("Marcus")
+// sayMyName("Marcus")
+// sayMyName("Marcus")
+// sayMyName("Marcus")
+// sayMyName("Marcus")
+
+// const notaFinal = 7
+
+// if(notaFinal < 5){
+//   document.write("Reprovado")
+// } else {
+//   document.write("Aprovado")
+// }
+
+// for (i = 0; i < 10; i++){
+//   document.write(`<p>${i}</p>`)
+// }
+
+
+
+
