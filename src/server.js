b9e0478a8dac3b3db04af034949af2ced3e3a6a4
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2020-06-06 13:09:21 -0300

    Adicionando novas rotas e atualizando arquivos HTML

--- src/server.js
+++ src/server.js
@@ -1,8 +1,18 @@
 const express = require("express")
 const server = express()
 
+server.use(express.static("public"))
+
 server.get("/", (req, res) => {
     res.sendfile(__dirname + "/views/index.html")
+})
+
+server.get("/create-point", (req, res) => {
+    res.sendfile(__dirname + "/views/create-point.html")
+})
+
+server.get("/search-results", (req, res) => {
+    res.sendfile(__dirname + "/views/search-results.html")
 })
 
 server.listen(3000)
