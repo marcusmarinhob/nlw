e9c552c1e587d173f153debdafcac755b1d395ba
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2020-06-06 22:06:13 -0300

    Cadastrando dados no banco de dados via formulário

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ const db = require("./database/db")
 
 server.use(express.static("public"))
 
+server.use(express.urlencoded({ extended: true}))
+
 const nunjucks = require("nunjucks")
 
 nunjucks.configure("src/views", {
@@ -18,6 +20,46 @@ server.get("/", (req, res) => {
 
 server.get("/create-point", (req, res) => {
     return res.render("create-point.html")
+})
+
+server.post("/create-point", (req, res) => {
+    // URL's Query Strings
+    console.log(req.body)
+
+    // Insert data in the table
+    const query = `
+    INSERT INTO places (
+        image,
+        name,
+        address,
+        address2,
+        state,
+        city,
+        items
+        ) VALUES (?,?,?,?,?,?,?);`
+
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
+
+    function afterInsertData(err){
+        if(err){
+            return console.log(err)
+        }
+
+        console.log("Cadastrado com sucesso")
+        console.log(this)
+        return res.send("ok")
+    }      
+
+    db.run(query, values, afterInsertData)
+
 })
 
 server.get("/search-results", (req, res) => {
